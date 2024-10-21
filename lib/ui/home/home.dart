6a82f30169d71581a5574385941cc30776c87a4a
HieuNMFrovis <hieunm.frovis@gmail.com>
2024-10-21 19:51:30 +0700

    code app MussicApp

---
+++ lib/ui/home/home.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+import 'package:mucsic_app/data/model/song.dart';
+import 'package:mucsic_app/ui/discovery/discovery.dart';
+import 'package:mucsic_app/ui/home/viewmodel.dart';
+import 'package:mucsic_app/ui/settings/settings.dart';
+import 'package:mucsic_app/ui/uses/user.dart';
+
+class MussicApp extends StatelessWidget {
+  const MussicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Mussic App",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MussicHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MussicHomePage extends StatefulWidget {
+  const MussicHomePage({super.key});
+
+  @override
+  State<MussicHomePage> createState() => _MussicHomePageState();
+}
+
+class _MussicHomePageState extends State<MussicHomePage> {
+  final List<Widget> _tabs = [
+    const HomeTab(),
+    const DiscoveryTab(),
+    const AccountTab(),
+    const SettingsTab(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: const CupertinoNavigationBar(
+          middle: Text('Mussic'),
+        ),
+        child: CupertinoTabScaffold(
+          tabBar: CupertinoTabBar(
+            backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.album), label: "Discovery"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person), label: "Account"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings), label: "Setting")
+            ],
+          ),
+          tabBuilder: (BuildContext context, int index) {
+            return _tabs[index];
+          },
+        ));
+  }
+}
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomeTabPage();
+  }
+}
+
+class HomeTabPage extends StatefulWidget {
+  const HomeTabPage({super.key});
+
+  @override
+  State<HomeTabPage> createState() => _HomeTabPageState();
+}
+
+class _HomeTabPageState extends State<HomeTabPage> {
+  List<Song> songs = [];
+  late MusicAppViewModel _viewModel;
+
+  @override
+  void initState() {
+    _viewModel = MusicAppViewModel();
+    _viewModel.loadSongs();
+    observerData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: getBody(),
+    );
+  }
+
+  void dispoes() {
+    _viewModel.songStream.close();
+    super.dispose();
+  }
+
+  Widget getBody() {
+    bool showLoading = songs.isEmpty;
+    if (showLoading) {
+      return getProgressBar();
+    } else {
+      return getListView();
+    }
+  }
+
+  Widget getProgressBar() {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  ListView getListView() {
+    // có đường phân tách ở giữa các phần tử thì sẽ dùng separated.
+    return ListView.separated(
+      itemBuilder: (context, position) {
+        return getRow(position);
+      },
+      separatorBuilder: (context, index) {
+        return const Divider(
+          color: Colors.grey,
+          thickness: 1,
+          indent: 24,
+          endIndent: 24,
+        );
+      },
+      itemCount: songs.length,
+      shrinkWrap: true,
+    );
+  }
+
+  Widget getRow(int index) {
+    return _SongItemSection(
+      song: songs[index],
+      parent: this,
+    );
+  }
+
+  void observerData() {
+    _viewModel.songStream.stream.listen((songList) {
+      setState(() {
+        songs.addAll(songList);
+      });
+    });
+  }
+}
+
+class _SongItemSection extends StatelessWidget {
+  const _SongItemSection({required this.song, required this.parent});
+  final _HomeTabPageState parent;
+  final Song song;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: FadeInImage.assetNetwork(
+          placeholder: 'assets/itune.png',
+          image: song.image,
+          width: 48,
+          height: 48,
+          imageErrorBuilder: (context, error, stackTrace) {
+            return Image.asset(
+              "assets/itune.png",
+              width: 48,
+              height: 48,
+            );
+          }),
+      title: Text(song.title),
+      subtitle: Text(song.artist),
+      trailing: IconButton(onPressed: () {}, icon: Icon(Icons.more_horiz)),
+    );
+  }
+}
